1bc86d2925dd27591dd1c13d6d1ffad4fed3c6f0
mikeqiu2011 <mikeqiu2011@163.com>
2020-06-25 22:47:33 +0800

    add locationMatch to set cache-control for images/ CSS/ JS

--- node/app.js
+++ node/app.js
@@ -5,16 +5,12 @@ const serveStatic = require("serve-static");
 
 app.use(
   serveStatic(path.join(__dirname, "public"), {
-    maxAge: "1d",
+    // maxAge: "1d",
   })
 );
 
 app.get("/", (req, res) => {
   console.log("/ has been accessed");
-
-  // res.setHeader("Cache-Control", "public, max-age=2592000");
-  // res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
-
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
